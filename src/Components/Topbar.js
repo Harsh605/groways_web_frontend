f543de5431431de314f1119b88cbf906f9f5b7ca
harsh <harshguptag1999@gmail.com>
2024-02-05 15:24:17 +0530

    change_links

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -14,14 +14,14 @@ const Footers = () => {
                 </div> */}
             </div>
             <div className="top-bar-right md:flex sm:justify-center sm:mx-0">
-                <a href="mailto:contact@dfi.com" target="_blank"><i className="fa-solid fa-envelope"></i></a>
-                <a href="https://www.facebook.com/profile.php?id=100093479117440" target="_blank"><i className="fa-brands fa-facebook"></i></a>
-                <a href="https://www.linkedin.com/in/buff-goofy-164bb027a" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-                <a href="https://www.instagram.com/buff.goofy/" target="_blank"><i className="fa-brands fa-instagram"></i></a>
-                <a href="https://twitter.com/findoutsoon" target="_blank"><i className="fa-brands fa-twitter"></i></a>
-                <a href="http://discordapp.com/users/1118422230804725760" target="_blank"><i className="fa-brands fa-discord"></i></a>
-                <a href="https://www.quora.com/profile/Buff-Goofy" target="_blank"><i className="fa-brands fa-quora"></i></a>
-                <a href="https://www.reddit.com/user/Buff_Goofy/" target="_blank"><i className="fa-brands fa-reddit"></i></a>
+                <a href="mailto:contact@Groways.com" target="_blank"><i className="fa-solid fa-envelope"></i></a>
+                <a href="" ><i className="fa-brands fa-facebook"></i></a>
+                <a href="" ><i className="fa-brands fa-linkedin"></i></a>
+                <a href="" ><i className="fa-brands fa-instagram"></i></a>
+                <a href="" ><i className="fa-brands fa-twitter"></i></a>
+                <a href="" ><i className="fa-brands fa-discord"></i></a>
+                <a href="" ><i className="fa-brands fa-quora"></i></a>
+                <a href=""><i className="fa-brands fa-reddit"></i></a>
                 {/* <a href="https://twitter.com/Metablocktech"   target="_blank"><i className="fa-brands fa-medium"></i></a> */}
                 {/* <a href="https://twitter.com/Metablocktech"   target="_blank"><i className="fa-brands fa-youtube"></i></a> */}
 
