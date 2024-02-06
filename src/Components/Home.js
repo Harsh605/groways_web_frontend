a3eccd5573a4856a1487ba69dedd9060feadbbad
harsh <harshguptag1999@gmail.com>
2024-02-06 21:43:16 +0530

    buildCode1

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -46,7 +46,7 @@ const Home = (props) => {
     {/* <RegisterinForsageBUSDPage/> */}
     {/* <SimpleSlider/> */}
     {/* <Autoplay/> */}
-    <Accountpreview/>
+    {/* <Accountpreview/> */}
     {/* <Roadmap/> */}
     <TokenIntro/>
     <HowToBuy/>
