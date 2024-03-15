c9cce8c2275dd7b1d9c3a0081378e90a9e0583ae
harsh <harshguptag1999@gmail.com>
2024-03-15 11:59:21 +0530

    socialLinks

--- src/Components/Needhelp.jsx
+++ src/Components/Needhelp.jsx
@@ -7,7 +7,7 @@ const Needhelp = () => {
             <div className="Need-help-container">
                 <h1>Need help with using the platform?</h1>
                 <p>Get qualified support from GroWays experts via online chat</p>
-                <button>Contact Support</button>
+                <button ><a target='_blank' style={{textDecoration:"none"}} href="mailto:contact@Groways.com">Contact Support</a></button>
 
             </div>
         </>
