10392cac6cf518e0ce5733be672994e314e7b191
devanshugahlot <devanshugahlot79@gmail.com>
2024-03-19 17:10:06 +0530

    new website update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Components/Home';
-import Navbar from './Components/Navbar';
-import './App.css';
-import AuthLayout from './OutletAndLayout/AuthLayout';
-import Layout from './OutletAndLayout/Layout';
-import RegisterinForsageBUSDPage from './Components/RegisterinForsageBUSDPage';
-import UserAccount from './Components/UserAccount'
-import AllUsers from './Components/AllUsers';
-import AllReferrals from './Components/AllReferrals';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import AuthLayout from "./OutletAndLayout/AuthLayout";
+import Layout from "./OutletAndLayout/Layout";
+import RegisterinForsageBUSDPage from "./Components/RegisterinForsageBUSDPage";
+import UserAccount from "./Components/UserAccount";
+import AllUsers from "./Components/AllUsers";
+import AllReferrals from "./Components/AllReferrals";
+import Show from "./Components/Show";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         {/* Routes for the AuthLayout */}
-        <Route path='/users/:userId' element={<UserAccount />} />
-        <Route path='/allusers/:address' element={<AllUsers />} />
-        <Route path='/referrals/:address' element={<AllReferrals />} />
+        <Route path="/users/:userId" element={<UserAccount />} />
+        <Route path="/allusers/:address" element={<AllUsers />} />
+        <Route path="/referrals/:address" element={<AllReferrals />} />
         <Route path={`/*`} element={<AuthLayout />}>
-          <Route path="RegisterinForsageBUSDPage" element={<RegisterinForsageBUSDPage />} />
+          <Route
+            path="RegisterinForsageBUSDPage"
+            element={<RegisterinForsageBUSDPage />}
+          />
+          <Route path="Show" element={<Show />} />
         </Route>
 
-        {/* Routes for the Layout */}
         <Route index element={<Home />} />
       </Routes>
     </Router>
--- src/Components/Needhelp.jsx
+++ src/Components/Needhelp.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import './Needhelp.css'
+import React from "react";
+import "./Needhelp.css";
 
 const Needhelp = () => {
-    return (
-        <>
-            <div className="Need-help-container">
-                <h1>Need help with using the platform?</h1>
-                <p>Get qualified support from GroWays experts via online chat</p>
-                <button ><a target='_blank' style={{textDecoration:"none"}} href="mailto:contact@Groways.com">Contact Support</a></button>
+  return (
+    <>
+      <div className="w-full my-5">
+        <h2
+          style={{ color: "white" }}
+          className="text-center font-bold text-3xl"
+        >
+          <span className="text-primary">User Friendly</span> Dashboard
+        </h2>
+        <div className="mt-4 w-full flex justify-center items-center p-4 max-w-3xl mx-auto">
+          <img
+            alt="Dashboad inside Laptop looks"
+            loading="lazy"
+            width={1920}
+            height={1080}
+            decoding="async"
+            data-nimg={1}
+            style={{ color: "transparent" }}
+            src="./images/Modern_illustrated_Programming_and_Software_Instagram_Post___1_-removebg-preview.png"
+          />
+        </div>
+      </div>
+      <div className="Need-help-container">
+        <h1>Need help with using the platform?</h1>
+        <p>Get qualified support from GroWays experts via online chat</p>
+        <button>
+          <a
+            target="_blank"
+            style={{ textDecoration: "none" }}
+            href="mailto:contact@Groways.com"
+          >
+            Contact Support
+          </a>
+        </button>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Needhelp
+export default Needhelp;
