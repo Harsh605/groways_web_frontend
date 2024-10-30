1de874b1206048da7cb90d7618ec04773ad8fa5b
harsh gupta <118052863+Harsh605@users.noreply.github.com>
2024-10-30 17:47:18 +0530

    Update RegisterinForsageBUSD.jsx

--- src/Components/RegisterinForsageBUSD.jsx
+++ src/Components/RegisterinForsageBUSD.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect  } from "react";
 import "./RegisterinForsageBUSD.css";
 import { useAccount } from "wagmi";
 import { useNavigate } from "react-router-dom";
